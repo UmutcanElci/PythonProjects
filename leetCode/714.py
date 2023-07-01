706001cdf3d5ebd98391e370e262e9d7c775ec10
Umutcan Elçi <umutcanelci@yaani.com>
2023-07-01 13:11:10 +0300

    Leet Code Problems

---
+++ leetCode/714.py
@@ -0,0 +1,18 @@
+class StockTrader:
+    def maxProfit(self, prices: list[int], fee: int) -> int:
+        buy = 0
+        sell = 0
+        profit = 0
+        
+        for i in prices:
+            
+        
+
+
+trader = StockTrader()
+
+
+prices = [1, 3, 2, 8, 4, 9]
+fee = 2
+profit = trader.maxProfit(prices, fee)
+print(profit)
