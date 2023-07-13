a94961c79a0588daa8857c9d5d4fa500362d9b3c
Umutcan Elçi <umutcanelci@yaani.com>
2023-07-13 16:10:57 +0300

    Django Tutorial

--- Django/crm/accounts/models.py
+++ Django/crm/accounts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length= 100, null=True)
+    def __str__(self) -> str:
+        return self.name
+
 
 class Customer(models.Model):
     name = models.CharField(max_length=100,null=True)
@@ -22,6 +27,10 @@ class Product(models.Model):
     category = models.CharField(max_length= 20,null=True,choices=CATEGORY)
     description = models.CharField(max_length=300,null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self) -> str:
+        return self.name
     
 
 
@@ -31,10 +40,10 @@ class Order(models.Model):
         ('Out for delivery','Out for delivery'),
         ('Delivered','Delivered'),
     )
-    #customer =
-    #product = 
+    customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50,null=True,choices=STATUS)
-    
+   
     
     
