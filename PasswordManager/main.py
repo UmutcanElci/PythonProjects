675258521b27934b946755064cd94ebc01feca2e
Umutcan Elçi <umutcanelci@yaani.com>
2023-10-15 20:05:58 +0300

    Save function

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -3,6 +3,21 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    
+    with open("data.txt","a") as data_file:
+        data_file.write(f"{website}  |  {email}  |  {password}\n")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)   
+    
+    
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -30,15 +45,17 @@ password_label.grid(row=3,column=0)
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1,column=1,columnspan=2, sticky="EW")
+website_entry.focus() 
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1,columnspan=2, sticky="EW")
+email_entry.insert(0,"someonesmail@outlook.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3,column=1, sticky="EW")
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3,column=2, sticky="EW")
-add_button = Button(text="Add",width=36)
+add_button = Button(text="Add",width=36,command=save)
 add_button.grid(row=4,column=1,columnspan=2, sticky="EW")
 
 
