238e27c3e8137bfa3b56e082fda7ba9249f7dc57
Umutcan Elçi <umutcanelci@yaani.com>
2023-10-15 21:33:47 +0300

    Message Box and pop ups

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,14 +10,20 @@ def save():
     password = password_entry.get()
     
     
-    with open("data.txt","a") as data_file:
-        data_file.write(f"{website}  |  {email}  |  {password}\n")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)   
-    
-    
-
-
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops",message="Please make sure you haven't left any fields empty. ")
+    else:
+        is_ok =  messagebox.askokcancel(title=website,message=f"These are the details entered : 
+                           \nEmail : {email} 
+                           \nPassword : {password} 
+                           \n Is it ok to save?")
+    if is_ok:
+        with open("data.txt","a") as data_file:
+         data_file.write(f"{website}  |  {email}  |  {password}\n")
+         website_entry.delete(0,END)
+         password_entry.delete(0,END)
+         
+         
 
 # ---------------------------- UI SETUP ------------------------------- #
 
