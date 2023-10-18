a71a5d550899442149e6ee91ae23567347fc058f
Umutcan Elçi <umutcanelci@yaani.com>
2023-10-18 12:34:28 +0300

    Json and try-catch

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
-# ---------------------------- PASSWORD GENERATOR ------------------------------- 
+import json
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -31,19 +32,35 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    
+    new_data = {website: {
+        "email": email,
+        "password": password,
+        }
+                }
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops",message="Please make sure you haven't left any fields empty. ")
     else:
-        is_ok =  messagebox.askokcancel(title=website,
-        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?"
-)
-    if is_ok:
-        with open("data.txt","a") as data_file:
-         data_file.write(f"{website}  |  {email}  |  {password}\n")
-         website_entry.delete(0,END)
-         password_entry.delete(0,END)
+        try:
+         with open("data.json","r") as data_file:
+            data = json.load(data_file)
+            data.update(new_data)
+        except FileNotFoundError:
+             with open("data.json","w") as data_file:
+                json.dump(new_data,data_file,indent=4)
+        else:
+            data.update(new_data)
+            
+            
+            with open("data.json","w") as data_file:
+                json.dump(data,data_file,indent=4)
+        finally:
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+             
+        
+         
+       
          
          
 
