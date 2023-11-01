e8cfd1fdd49daf9d1b106afa5cc2bb6904dea781
Umutcan Elçi <umutcanelci@yaani.com>
2023-11-01 13:31:07 +0300

    Adding class name and conf label

--- YoloTry/YoutubeTutorial/Runnig_YOLO_Webcam/main.py
+++ YoloTry/YoutubeTutorial/Runnig_YOLO_Webcam/main.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import cv2
+import math
+
 
 cap=cv2.VideoCapture(0)
 
@@ -11,6 +13,20 @@ frame_height = int(cap.get(4))
 
 model = YOLO("../YOLO_Weights/yolov8n.pt")
 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+
+
 while True:
     succes, img = cap.read()
     results = model(img,stream=True)
@@ -20,7 +36,20 @@ while True:
         for box in boxes:
             x1,y1,x2,y2 = box.xyxy[0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            print(x1,y1,x2,y2)
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,0),2)
+            
+            conf = math.ceil((box.conf[0]*100))/100
+            cls = int(box.cls[0])
+            class_name=classNames[cls]
+            label = f"{class_name}{conf}"
+            t_Size = cv2.getTextSize(label, 0 , fontScale=1,thickness=2)[0]
+            
+            c2 = x1 + t_Size[0], y1 - t_Size[1] - 3
+            cv2.rectangle(img, (x1,y1), c2,[255,0,255],-1,cv2.LINE_AA)
+            cv2.putText(img,label,(x1,y1-2),0,1,[255,255,255],thickness=1,lineType=cv2.LINE_AA)
+            
+            
     cv2.imshow("Image",img)
     if cv2.waitKey(1) & 0xff== ord("1"):
        break
